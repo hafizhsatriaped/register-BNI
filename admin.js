aac5031e795d6cd313b7853bd7b5e45147a2a77e
hafizhsatriaped <hafizhsatria@gmail.com>
2026-08-06 01:12:32 +0700

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,3 +1,7 @@
+window.SUPABASE_URL = "https://tadmzfqxfpbcurybcjet.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_hnIk56jX8MWCoq7-tQb30g_SI7ammBm";
+window.ADMIN_KEY = "Joni18munir21";
+
 (function () {
   // --- Akses kontrol dengan key dari config.js ---
   const urlParams = new URLSearchParams(window.location.search);
